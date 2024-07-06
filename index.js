a9155dd5233d5da10a3998df5b2875a2a11aafd1
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-07-06 18:43:57 +0200

    first lesson

---
+++ index.js
@@ -0,0 +1,45 @@
+/*
+  ? Дано 90 хвилин, дізнайтесь скільки в даному числі міститься годин і хвилин.
+  ? Результат виведіть у консоль.
+*/
+
+// const time = 903;
+// const hours = Math.floor(time / 60);
+// const updatedHours = hours <= 9 ? `0${hours}` : `${hours}`;
+// const minutes = (time % 60).toString().padStart(2, "0");
+
+// console.log(`${updatedHours}:${minutes}`);
+
+// ------------------------------------------
+
+// const str = "24px";
+// console.log(parseFloat(str));
+
+// console.log(NaN == NaN)
+// ------------------------------------------
+
+// const smNumber = NaN;
+// console.log(typeof smNumber);
+// console.log(Number.isNaN(smNumber));
+// console.log(smNumber !== smNumber);
+
+// console.log(Math.max(1, 2, 3, 4, 6));
+
+// const str = "Hello world";
+// str.replace("l", "r");
+
+// console.log(str.indexOf("l"));
+//
+
+// console.log(3 > 2 > 1);
+// console.log(true == 1);
+// console.log(null > 0);
+// console.log(null == 0);
+
+// console.log(null >= 0);
+
+// console.log(false && true);
+
+// console.log(false || false);
+
+// console.log( ?? );
