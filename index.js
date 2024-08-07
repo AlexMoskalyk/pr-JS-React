17cd1e7bd040fd7a0b5544692bcaf14153a82fae
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-08-07 18:07:53 +0200

    train skiderClass Vanilla js

--- index.js
+++ index.js
@@ -295,93 +295,156 @@
 // console.log(someGreaterThanTen);
 // -------------------------------
 
-const people = [
-  {
-    name: "Alex",
-    know: ["Alex", "Jhon"],
-  },
-  {
-    name: "Jhon",
-    know: [],
-  },
-  {
-    name: "Eva",
-    know: ["Alex", "Jhon"],
-  },
-  {
-    name: "Ivan",
-    know: ["Jhon", "Eva"],
-  },
-];
+// const people = [
+//   {
+//     name: "Alex",
+//     know: ["Alex", "Jhon"],
+//   },
+//   {
+//     name: "Jhon",
+//     know: [],
+//   },
+//   {
+//     name: "Eva",
+//     know: ["Alex", "Jhon"],
+//   },
+//   {
+//     name: "Ivan",
+//     know: ["Jhon", "Eva"],
+//   },
+// ];
 //нарцис  'Jhon'
 
-const people1 = [
-  {
-    name: "Alex",
-    know: ["Alex", "Jhon"],
-  },
-  {
-    name: "Jhon",
-    know: [],
-  },
-  {
-    name: "Eva",
-    know: [],
-  },
-  {
-    name: "Ivan",
-    know: ["Jhon", "Eva"],
-  },
-];
+// const people1 = [
+//   {
+//     name: "Alex",
+//     know: ["Alex", "Jhon"],
+//   },
+//   {
+//     name: "Jhon",
+//     know: [],
+//   },
+//   {
+//     name: "Eva",
+//     know: [],
+//   },
+//   {
+//     name: "Ivan",
+//     know: ["Jhon", "Eva"],
+//   },
+// ];
 //немає нарциса'
 
-const people3 = [
-  {
-    name: "Alex",
-    know: ["Alex", "Eva"],
-  },
-  {
-    name: "Jhon",
-    know: [],
-  },
-  {
-    name: "Eva",
-    know: ["Alex", "Jhon"],
-  },
-  {
-    name: "Ivan",
-    know: ["Jhon", "Eva"],
-  },
-];
+// const people3 = [
+//   {
+//     name: "Alex",
+//     know: ["Alex", "Eva"],
+//   },
+//   {
+//     name: "Jhon",
+//     know: [],
+//   },
+//   {
+//     name: "Eva",
+//     know: ["Alex", "Jhon"],
+//   },
+//   {
+//     name: "Ivan",
+//     know: ["Jhon", "Eva"],
+//   },
+// ];
 //немає нарциса
 
-const people4 = [
-  {
-    name: "Alex",
-    know: ["Alex", "Jhon"],
-  },
-  {
-    name: "Jhon",
-    know: ["Eva"],
-  },
-  {
-    name: "Eva",
-    know: ["Alex", "Jhon"],
-  },
-  {
-    name: "Ivan",
-    know: ["Jhon", "Eva"],
-  },
-];
+// const people4 = [
+//   {
+//     name: "Alex",
+//     know: ["Alex", "Jhon"],
+//   },
+//   {
+//     name: "Jhon",
+//     know: ["Eva"],
+//   },
+//   {
+//     name: "Eva",
+//     know: ["Alex", "Jhon"],
+//   },
+//   {
+//     name: "Ivan",
+//     know: ["Jhon", "Eva"],
+//   },
+// ];
+
 //немає нарциса'
 //
 // Нарциса знають всі, нарцис незнає нікого
+// ================решение================
+// function findNarcis(people) {
+//   const knownCounts = new Map();
+//   const knowSomeone = new Set();
+
+//   for (const person of people) {
+//     if (!knownCounts.has(person.name)) {
+//       knownCounts.set(person.name, 0);
+//     }
+
+//     for (const knownPerson of person.know) {
+//       knownCounts.set(knownPerson, (knownCounts.get(knownPerson) || 0) + 1);
+//     }
+
+//     if (person.know.length > 0) {
+//       knowSomeone.add(person.name);
+//     }
+//   }
+
+//   for (const person of people) {
+
+//     if (
+//       knownCounts.get(person.name) === people.length - 1 &&
+//       !knowSomeone.has(person.name)
+//     ) {
+//       return person.name;
+//     }
+//   }
+
+//   return null;
+// }
+
+// function findNarcis(people) {
+//   const narcis = people.find((item) => item.know.length === 0);
+//   if (!narcis) {
+//     return "Нет нарциса";
+//   }
+
+//   const isEveryKnowNarcis = people.every((item) => {
+//     if (narcis.name === item.name) {
+//       return true;
+//     }
+
+//     return item.know.includes(narcis.name);
+//   });
+
+//   if (isEveryKnowNarcis) return narcis.name;
+//   return "Нет нарциса";
+// }
+
+// console.log(findNarcis(people));
+// console.log(findNarcis(people1));
+// console.log(findNarcis(people3));
+// console.log(findNarcis(people4));
+
+// =======================================
 
 // const name = "Mango";
 // console.log(name.__proto__);
 // console.log(String.prototype.__proto__);
 
 // console.log(Object.prototype === String.prototype.__proto__);
+// console.log(Object.prototype);
+// console.log(Object.__proto__);
+// console.log(Object.prototype.__proto__);
+// console.log(String.prototype);
+// console.log(String.__proto__);
+// console.log(String.prototype.__proto__);
 // =-----======================
 // Щоб розуміти, що це за __proto__, потрібно точно знати за допомогою якої функції-конструктора або класу був створений даний об'єкт
 // ================================
@@ -391,6 +454,7 @@ const people4 = [
 // const b = 34;
 
 // console.log(name.__proto__);
+// console.log(name.prototype);
 
 // function multiply() {} // new Function(...)
 // const fn = function () {}; // new Function(...)
@@ -404,6 +468,9 @@ const people4 = [
 //     return this.name;
 //   }
 // } // new Function(...)
+// console.log(User.prototype.__proto__);
+// console.log(Object.prototype);
+// console.log(User.prototype.__proto__ === Object.prototype);
 // const mango = new User("alex");
 // // console.log(mango.getName());
 // // =======================================
@@ -423,6 +490,10 @@ const people4 = [
 //
 
 // посмотреть кто главней Function.prototype или Object.prototype
+// console.log(Function.prototype.__proto__);
+// console.log(Function.prototype);
+// console.log(Function.__proto__);
+
 // console.log(Function.prototype.__proto__ === Object.prototype);
 // console.log(Function.prototype === Object.__proto__);
 
@@ -474,3 +545,40 @@ const people4 = [
 // console.log(age.__proto__.__proto__.__proto__ === Object.prototype.__proto__);
 // console.log(Number.prototype.__proto__ === Object.prototype);
 // console.log(Boolean.prototype.__proto__ === Object.prototype);
+
+// =========================================================
+
+/*
+У супермаркеті вишикувалась черга до кас самообслуговування. Існує лише ОДНА черга, і
+Порядок в черзі НІКОЛИ не змінюється, і
+Припустимо, що перша людина в черзі (тобто перший елемент масиву/списку) проходить до каси, як тільки вона звільняється.
+Ваша задача - написати функцію, яка обчислює загальний час, необхідний для того, щоб усі покупці змогли розрахуватись!
+Функція має дві вхідні змінні:
+customers: масив  натуральних чисел, що представляє чергу. Кожне ціле число представляє клієнта, а його значення - це час, необхідний для того, щоб виїхати.
+n: натуральне число, кількість кас.
+Функція повинна повернути ціле число - загальний час, необхідний для цього.
+Отже, наприклад
+queueTime([5,3,4], 1)
+// має повертати 12
+// тому що при n=1 загальний час дорівнює сумі часів
+queueTime([10,2,3,3], 2)
+// має повернути 10
+// тому що тут n=2 і 2-га, 3-тя і 4-та людина в черзі 
+// черзі закінчать раніше, ніж закінчить 1-а людина.
+queueTime([2,3,10], 2)
+// має повернути 12
+*/
+
+function queueTime(customers, n) {
+  const cases = new Array(n).fill(0);
+
+  for (const time of customers) {
+    const idx = cases.indexOf(Math.min(...cases));
+    cases[idx] += time;
+  }
+  console.log(Math.max(...cases));
+}
+
+queueTime([10, 2, 3, 3], 2);
+queueTime([5, 3, 4], 1);
+queueTime([2, 3, 10], 2);
